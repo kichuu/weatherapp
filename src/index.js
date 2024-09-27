dde34eeaa4c18fbf5d27f45f7df98e2df9f6b6a3
krishnadev <krishnadevkichus@gmail.com>
2024-09-27 14:21:14 +0530

    added try catch for error handling

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
 console.log("Hello World!");
-import "./styles.css"
-
+import "./styles.css";
 
 async function currentLocationFetch(locationGiven) {
-    let locationData = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=Q0abp83L6OAZwOcUZGLA2EPh90T4QhjG&q=${locationGiven}`)
-    let prossesedData = await locationData.json()
-    let currentLocationKey = prossesedData[0].Key
-    return currentLocationKey
+  try {
+    let locationData = await fetch(
+      `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=Q0abp83L6OAZwOcUZGLA2EPh90T4QhjG&q=${locationGiven}`
+    );
+    let prossesedData = await locationData.json();
+    let currentLocationKey = prossesedData[0].Key;
+    return currentLocationKey;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-
 async function CurrentWeatherConditionFetch(Location) {
-    let currentLocationKey = await currentLocationFetch(Location)
-    let currentWeatherData = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/${currentLocationKey}?apikey=Q0abp83L6OAZwOcUZGLA2EPh90T4QhjG`)
-    let WeatherDataProcessed = await currentWeatherData.json()
-    let tempOfLocationInCelsius = WeatherDataProcessed[0].Temperature.Metric.Value
-    let tempOfLocationInFarenheit = WeatherDataProcessed[0].Temperature.Imperial.Value
-    let weatherIcon = WeatherDataProcessed[0].WeatherIcon
-    let weatherText = WeatherDataProcessed[0].WeatherText
+  try {
+    let currentLocationKey = await currentLocationFetch(Location);
+    let currentWeatherData = await fetch(
+      `http://dataservice.accuweather.com/currentconditions/v1/${currentLocationKey}?apikey=Q0abp83L6OAZwOcUZGLA2EPh90T4QhjG`
+    );
+    let WeatherDataProcessed = await currentWeatherData.json();
+    let tempOfLocationInCelsius =
+      WeatherDataProcessed[0].Temperature.Metric.Value;
+    let tempOfLocationInFarenheit =
+      WeatherDataProcessed[0].Temperature.Imperial.Value;
+    let weatherIcon = WeatherDataProcessed[0].WeatherIcon;
+    let weatherText = WeatherDataProcessed[0].WeatherText;
     console.log(WeatherDataProcessed);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 const locationSearchBar = document.getElementById("location-search-bar");
-const locationSubmit = document.getElementById("location-submit")
+const locationSubmit = document.getElementById("location-submit");
 
 locationSubmit.addEventListener("click", () => {
-    let locationGiven = locationSearchBar.value; 
-    CurrentWeatherConditionFetch(locationGiven);
-})
+  let locationGiven = locationSearchBar.value;
+  CurrentWeatherConditionFetch(locationGiven);
+});
