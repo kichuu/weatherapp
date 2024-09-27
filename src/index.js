2e1989c688c0bf9813755d9d084bc233b63bd646
krishnadev <krishnadevkichus@gmail.com>
2024-09-27 16:08:56 +0530

    done

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./styles.css";
 
 async function currentLocationFetch(locationGiven) {
     let locationData = await fetch(
-      `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=Q0abp83L6OAZwOcUZGLA2EPh90T4QhjG&q=${locationGiven}`
+      `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=Q0abp83L6OAZwOcUZGLA2EPh90T4QhjG&q=${locationGiven}`
     , {  mode: 'cors'    } );
     let prossesedData = await locationData.json();
     let currentLocationKey = prossesedData[0].Key;
@@ -13,7 +13,7 @@ async function currentLocationFetch(locationGiven) {
 async function CurrentWeatherConditionFetch(Location) {
     let currentLocationKey = await currentLocationFetch(Location);
     let currentWeatherData = await fetch(
-      `http://dataservice.accuweather.com/currentconditions/v1/${currentLocationKey}?apikey=Q0abp83L6OAZwOcUZGLA2EPh90T4QhjG`
+      `https://dataservice.accuweather.com/currentconditions/v1/${currentLocationKey}?apikey=Q0abp83L6OAZwOcUZGLA2EPh90T4QhjG`
     ,{  mode: 'cors'    } );
     let WeatherDataProcessed = await currentWeatherData.json();
     let tempOfLocationInCelsius =
