fddd1514e2e1ef34c57a862b4edc7e40f3197114
krishnadev <krishnadevkichus@gmail.com>
2024-09-27 17:01:27 +0530

    added SoMeStYlE

--- src/index.js
+++ src/index.js
@@ -1,36 +1,31 @@
 console.log("Hello World!");
 import "./styles.css";
 
-async function currentLocationFetch(locationGiven) {
-    let locationData = await fetch(
-      `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=Q0abp83L6OAZwOcUZGLA2EPh90T4QhjG&q=${locationGiven}`
-    , {  mode: 'cors'    } );
-    let prossesedData = await locationData.json();
-    let currentLocationKey = prossesedData[0].Key;
-    return currentLocationKey;
-}
-
+// async function currentLocationFetch(locationGiven) {
+//     let locationData = await fetch(
+//       `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=Q0abp83L6OAZwOcUZGLA2EPh90T4QhjG&q=${locationGiven}`
+//     , {  mode: 'cors'    } );
+//     let prossesedData = await locationData.json();
+//     let currentLocationKey = prossesedData[0].Key;
+//     return currentLocationKey;
+// }
+let currentTemp = "234"
 async function CurrentWeatherConditionFetch(Location) {
-    let currentLocationKey = await currentLocationFetch(Location);
-    let currentWeatherData = await fetch(
-      `https://dataservice.accuweather.com/currentconditions/v1/${currentLocationKey}?apikey=Q0abp83L6OAZwOcUZGLA2EPh90T4QhjG`
-    ,{  mode: 'cors'    } );
+    // let currentLocationKey = await currentLocationFetch(Location);
+    let currentWeatherData = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${Location}?unitGroup=us&include=current&key=MMMPMMJZTQKPPFF9TZ4SB7ASN&contentType=json`,{  mode: 'cors'    } );
     let WeatherDataProcessed = await currentWeatherData.json();
-    let tempOfLocationInCelsius =
-      WeatherDataProcessed[0].Temperature.Metric.Value;
-    let tempOfLocationInFarenheit =
-      WeatherDataProcessed[0].Temperature.Imperial.Value;
-    let weatherIcon = WeatherDataProcessed[0].WeatherIcon;
-    let weatherText = WeatherDataProcessed[0].WeatherText;
-    console.log(WeatherDataProcessed);
+    currentTemp = WeatherDataProcessed.currentConditions.temp
+    console.log(WeatherDataProcessed)
+    console.log(currentTemp)
 }
 
 const locationSearchBar = document.getElementById("location-search-bar");
 const locationSubmit = document.getElementById("location-submit");
-
-locationSubmit.addEventListener("click", () => {
-  let locationGiven = locationSearchBar.value;
-  CurrentWeatherConditionFetch(locationGiven);
+const tempShow = document.querySelector(".temperature") 
+locationSubmit.addEventListener("click", async() => {
+  let Location = locationSearchBar.value;
+  await CurrentWeatherConditionFetch(Location);
+  tempShow.innerHTML = currentTemp +"°C"
 });
 
 
